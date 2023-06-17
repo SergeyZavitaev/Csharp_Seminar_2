9b51b4fccef17ba2ee33cff8a51c2f156e23577c
Sergey Zavitaev <nikgala@list.ru>
2023-06-17 18:17:41 +0300

    Added solution for 3 task

---
+++ Task_3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 5 && number < 8)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
