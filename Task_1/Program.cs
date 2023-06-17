2ddf4d113216e49baecad3fa9fa174a5ed3e2db0
Sergey Zavitaev <nikgala@list.ru>
2023-06-17 18:22:52 +0300

    Improve mistake with Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine((number / 10) % 10);
+if (number < 1000)
+    Console.WriteLine((number / 10) % 10);
+else
+    Console.WriteLine("Число должно быть трёхзначным");
